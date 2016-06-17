1d354ea70ce3caf3013dd128c836e44d7bdac9dd
shailaja183 <shailaja.sampat@yahoo.com>
2016-06-18 01:21:32 +0530

    Update donut.js

--- js/donut.js
+++ js/donut.js
@@ -15,11 +15,7 @@ var arc = d3.svg.arc()
 		.on("mouseover", function(d) {	
 					t.transition().duration(200).style("opacity", 1);
 					t.html("<font size=3>" + "131/232 seats obtained by AIADMK" + "</font>").style("left", 100).style("top", 100);
-					d3.select(".arc1").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc3").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc4").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc6").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc7").attr("fill","rgb(204,204,204)"); 
+					d3.selectAll(".arc1, .arc3, .arc4, .arc6, .arc7").attr("fill","rgb(204,204,204)"); 
 		})
 		.on("mouseout", function(d) {				
 					t.transition().duration(500).style("opacity", 0);
@@ -37,11 +33,7 @@ var arc1 = d3.svg.arc()
 					t.transition().duration(200).style("opacity", 1);
 					
 					t.html("<font size=3>" + "89/232 seats obtained by DMK" + "</font>").style("left", 100).style("top", 100);
-					d3.select(".arc").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc3").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc4").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc6").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc7").attr("fill","rgb(204,204,204)"); 
+					d3.selectAll(".arc, .arc3, .arc4, .arc6, .arc7").attr("fill","rgb(204,204,204)"); 
 		})
 		.on("mouseout", function(d) {				
 					t.transition().duration(500).style("opacity", 0);
@@ -69,10 +61,7 @@ var arc3 = d3.svg.arc()
 		.on("mouseover", function(d) {	
 					t.transition().duration(200).style("opacity", 1);
 					t.html("<font size=3>" + "40.8% of the contested vote share for AIADMK" + "</font>").style("left", 100).style("top", 100);
-					d3.select(".arc1").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc4").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc6").attr("fill","rgb(204,204,204)");
-					d3.select(".arc7").attr("fill","rgb(204,204,204)"); 
+					d3.selectAll(".arc1, .arc4, .arc6, .arc7").attr("fill","rgb(204,204,204)"); 
 		})
 		.on("mouseout", function(d) {				
 					t.transition().duration(500).style("opacity", 0);
@@ -88,11 +77,7 @@ var arc4 = d3.svg.arc()
 		.on("mouseover", function(d) {	
 					t.transition().duration(200).style("opacity", 1);
 					t.html("<font size=3>" + "41.1% of the contested vote share for DMK" + "</font>").style("left", 100).style("top", 100);
-					d3.select(".arc").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc3").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc6").attr("fill","rgb(204,204,204)");
-					d3.select(".arc7").attr("fill","rgb(204,204,204)");
-					
+					d3.selectAll(".arc, .arc3, .arc6, .arc7").attr("fill","rgb(204,204,204)");
 		})
 		.on("mouseout", function(d) {				
 					t.transition().duration(500).style("opacity", 0);
@@ -119,9 +104,7 @@ var arc6 = d3.svg.arc()
 		.on("mouseover", function(d) {	
 					t.transition().duration(200).style("opacity", 1);
 					t.html("<font size=3>" + "40.8% of the total vote share for AIADMK" + "</font>").style("left", 100).style("top", 100);
-					d3.select(".arc1").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc4").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc7").attr("fill","rgb(204,204,204)");
+					d3.selectAll(".arc1, .arc4, .arc7").attr("fill","rgb(204,204,204)");
 		})
 		.on("mouseout", function(d) {				
 					t.transition().duration(500).style("opacity", 0);
@@ -137,9 +120,7 @@ var arc7 = d3.svg.arc()
 					t.transition().duration(200).style("opacity", 1);
 					
 					t.html("<font size=3>" + "39.7% of the total vote share for DMK" + "</font>").style("left", 100).style("top", 100);
-					d3.select(".arc").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc3").attr("fill","rgb(204,204,204)"); 
-					d3.select(".arc6").attr("fill","rgb(204,204,204)");
+					d3.selectAll(".arc, .arc3, .arc6").attr("fill","rgb(204,204,204)");
 		})
 		.on("mouseout", function(d) {				
 					t.transition().duration(500).style("opacity", 0);
@@ -160,19 +141,93 @@ var arc8 = d3.svg.arc()
 					t.transition().duration(500).style("opacity", 0);
 		});
 
-var labs = [{val:"131",xpos:-15,ypos:70,col:"white",size:"15",ang:"rotate(-40)"},{val:"40.8 %",xpos:-15,ypos:92,col:"white",size:"15",ang:"rotate(-40)"},{val:"40.8 %",xpos:-15,ypos:115,col:"white",size:"15",ang:"rotate(-40)"},{val:"89",xpos:-15,ypos:70,col:"white",size:"15",ang:"rotate(40)"},{val:"41.1 %",xpos:-15,ypos:92,col:"white",size:"15",ang:"rotate(40)"},{val:"39.7 %",xpos:-15,ypos:117,col:"white",size:"15",ang:"rotate(40)"} ];
+var tx = svg.append("text").text("131").attr("x",-15).attr("y",70).attr("font-size",15).attr("fill","white").attr("transform","rotate(-40)").attr("font-family","Roboto Condensed")
+		.on("mouseover", function(d) {	
+					t.transition().duration(200).style("opacity", 1);
+					t.html("<font size=3>" + "131/232 seats obtained by AIADMK" + "</font>").style("left", 100).style("top", 100);
+					d3.selectAll(".arc1, .arc3, .arc4, .arc6, .arc7").attr("fill","rgb(204,204,204)"); 
+		})
+		.on("mouseout", function(d) {				
+					t.transition().duration(500).style("opacity", 0);
+					d3.select(".arc1").attr("fill","#ff0000"); 
+					d3.select(".arc3").attr("fill","#33cc33"); 
+					d3.select(".arc4").attr("fill","#ff4d4d"); 
+					d3.select(".arc6").attr("fill","#70db70"); 
+					d3.select(".arc7").attr("fill","#ff8080"); 
+		});
+		
+var tx3 = svg.append("text").text("40.8%").attr("x",-15).attr("y",92).attr("font-size",15).attr("fill","white").attr("transform","rotate(-40)").attr("font-family","Roboto Condensed")
+		.on("mouseover", function(d) {	
+					t.transition().duration(200).style("opacity", 1);
+					t.html("<font size=3>" + "40.8% of the contested vote share for AIADMK" + "</font>").style("left", 100).style("top", 100);
+					d3.selectAll(".arc1, .arc4, .arc6, .arc7").attr("fill","rgb(204,204,204)"); 
+		})
+		.on("mouseout", function(d) {				
+					t.transition().duration(500).style("opacity", 0);
+					d3.select(".arc1").attr("fill","#ff0000"); 
+					d3.select(".arc4").attr("fill","#ff4d4d"); 
+					d3.select(".arc7").attr("fill","#ff8080");
+					d3.select(".arc6").attr("fill","#70db70");
+		});
+		
+var tx6 = svg.append("text").text("40.8%").attr("x",-15).attr("y",115).attr("font-size",15).attr("fill","white").attr("transform","rotate(-40)").attr("font-family","Roboto Condensed")
+		.on("mouseover", function(d) {	
+					t.transition().duration(200).style("opacity", 1);
+					t.html("<font size=3>" + "40.8% of the total vote share for AIADMK" + "</font>").style("left", 100).style("top", 100);
+					d3.selectAll(".arc1, .arc4, .arc7").attr("fill","rgb(204,204,204)");
+		})
+		.on("mouseout", function(d) {				
+					t.transition().duration(500).style("opacity", 0);
+					d3.select(".arc1").attr("fill","#ff0000"); 
+					d3.select(".arc4").attr("fill","#ff4d4d"); 
+					d3.select(".arc7").attr("fill","#ff8080");
+		});
 
-svg.selectAll("text")
-		   .data(labs).enter().append("text")
-		   .text(function(d){ return d.val; })
-		   .attr("y", function(d){ return d.ypos; })
-		   .attr("x", function(d){ return d.xpos; })
-		   .attr("font-size",function(d){ return d.size; })
-		   .attr("fill",function(d){ return d.col; })
-		   .attr("transform", function(d){ return d.ang; })
-		   .attr("font-family","Roboto Condensed");
-		   
-		$( "#tooltip" ).position({
+var tx1 = svg.append("text").text("89").attr("x",-15).attr("y",70).attr("font-size",15).attr("fill","white").attr("transform","rotate(40)").attr("font-family","Roboto Condensed")
+		.on("mouseover", function(d) {	
+					t.transition().duration(200).style("opacity", 1);
+					
+					t.html("<font size=3>" + "89/232 seats obtained by DMK" + "</font>").style("left", 100).style("top", 100);
+					d3.selectAll(".arc, .arc3, .arc4, .arc6, .arc7").attr("fill","rgb(204,204,204)"); 
+		})
+		.on("mouseout", function(d) {				
+					t.transition().duration(500).style("opacity", 0);
+					d3.select(".arc").attr("fill","#29a329"); 
+					d3.select(".arc3").attr("fill","#33cc33");
+					d3.select(".arc4").attr("fill","#ff4d4d");
+					d3.select(".arc6").attr("fill","#70db70"); 
+					d3.select(".arc7").attr("fill","#ff8080");		
+		});
+		
+var tx4 = svg.append("text").text("41.1%").attr("x",-15).attr("y",92).attr("font-size",15).attr("fill","white").attr("transform","rotate(40)").attr("font-family","Roboto Condensed")
+		.on("mouseover", function(d) {	
+					t.transition().duration(200).style("opacity", 1);
+					t.html("<font size=3>" + "41.1% of the contested vote share for DMK" + "</font>").style("left", 100).style("top", 100);
+					d3.selectAll(".arc, .arc3, .arc6, .arc7").attr("fill","rgb(204,204,204)");
+		})
+		.on("mouseout", function(d) {				
+					t.transition().duration(500).style("opacity", 0);
+					d3.select(".arc").attr("fill","#29a329"); 
+					d3.select(".arc3").attr("fill","#33cc33"); 
+					d3.select(".arc6").attr("fill","#70db70");
+					d3.select(".arc7").attr("fill","#ff8080");
+		});
+
+var tx7 = svg.append("text").text("39.7%").attr("x",-15).attr("y",117).attr("font-size",15).attr("fill","white").attr("transform","rotate(40)").attr("font-family","Roboto Condensed")
+		.on("mouseover", function(d) {	
+					t.transition().duration(200).style("opacity", 1);
+					
+					t.html("<font size=3>" + "39.7% of the total vote share for DMK" + "</font>").style("left", 100).style("top", 100);
+					d3.selectAll(".arc, .arc3, .arc6").attr("fill","rgb(204,204,204)");
+		})
+		.on("mouseout", function(d) {				
+					t.transition().duration(500).style("opacity", 0);
+					d3.select(".arc").attr("fill","#29a329"); 
+					d3.select(".arc3").attr("fill","#33cc33"); 
+					d3.select(".arc6").attr("fill","#70db70");
+		});
+
+$( "#tooltip" ).position({
   my: "center",
   at: "center",
   of: "#donut"
